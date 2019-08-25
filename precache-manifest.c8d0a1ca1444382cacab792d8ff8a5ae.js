0c6cadbca729f068f312f4b28aafb4a8e52eeb27
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2019-08-25 11:27:19 -0500

    Updates

---
+++ precache-manifest.c8d0a1ca1444382cacab792d8ff8a5ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb88e275d3cebc28011d6d270cba534",
+    "url": "/yuuvis-marketing-app/index.html"
+  },
+  {
+    "revision": "a69d806975474f250e47",
+    "url": "/yuuvis-marketing-app/static/css/main.dc6d572f.chunk.css"
+  },
+  {
+    "revision": "d3e4d16b8d3f9fac24d0",
+    "url": "/yuuvis-marketing-app/static/js/2.9faeca98.chunk.js"
+  },
+  {
+    "revision": "a69d806975474f250e47",
+    "url": "/yuuvis-marketing-app/static/js/main.22b46ea4.chunk.js"
+  },
+  {
+    "revision": "d219e9c6c0acf582d81c",
+    "url": "/yuuvis-marketing-app/static/js/runtime~main.04120b3a.js"
+  },
+  {
+    "revision": "db1a9e5a79fff936390cb5e519f36291",
+    "url": "/yuuvis-marketing-app/static/media/hackathon.db1a9e5a.jpg"
+  }
+]);
